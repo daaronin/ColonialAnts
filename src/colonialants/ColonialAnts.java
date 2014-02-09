3afdfb43399cd0fbc713945f20cad55e30ae2227
George <georgemcdaid@gmail.com>
2014-02-09 06:02:38 -0500

    Init Commit

---
+++ src/colonialants/ColonialAnts.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package colonialants;
+
+/**
+ *
+ * @author George McDaid
+ */
+public class ColonialAnts {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
